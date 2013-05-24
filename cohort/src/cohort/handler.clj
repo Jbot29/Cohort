e04e75b18cae5138b0988d0be17f39e33cfa3145
Jeremy Schwartz <jeremy.schwartz@mapmyfitness.com>
2013-05-24 09:53:36 -0500

    mustache handler

--- cohort/src/cohort/handler.clj
+++ cohort/src/cohort/handler.clj
@@ -2,13 +2,25 @@
   (:use compojure.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
-	    [monger.core :as mg]))
+	    [monger.core :as mg]
+	    [clostache.parser :as clostache]))
 
 (mg/connect!)
 (mg/set-db! (mg/get-db "monger-test"))
 
+(defn read-template [template-name]
+  (slurp
+      (str "resources/public/templates/" template-name ".mustache")))
+
+
+(defn render-template [template-file params]
+  (clostache/render (read-template template-file) params))
+
+(defn index []
+  (render-template "index" {:greeting "Bonjour"}))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (index))
   (route/resources "/")
   (route/not-found "Not Found"))
 
