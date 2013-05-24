f20f1b77225f2771bdf5e37960116b7e91221fb1
Jeremy Schwartz <jeremy.schwartz@mapmyfitness.com>
2013-05-24 08:40:11 -0500

    added monger and mustache

--- cohort-noir/project.clj
+++ cohort-noir/project.clj
@@ -1,6 +1,8 @@
 (defproject cohort-noir "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.3.0-beta3"]]
+                           [noir "1.3.0-beta3"]
+			   [com.novemberain/monger "1.5.0"] 
+			   [de.ubercode.clostache/clostache "1.3.1"]]
             :main cohort-noir.server)
 
