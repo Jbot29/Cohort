ece8b4de5797e79016bd4f9c5354c0e994acde4f
Carl Youngblood <carl.youngblood@its-macbook-pro.mapmyfitness.local>
2013-05-24 16:32:58 -0500

    blargh

--- cohort/src/cohort/mongo.clj
+++ cohort/src/cohort/mongo.clj
@@ -9,12 +9,21 @@
 (defn score-collection [challenge-id score-name]
   (str "challenge_" challenge-id "_" score-name "_scores"))
 
-(defn daily-score-cursor [challenge-id score-name]
+(defn joined [user-id challenge-id]
+  (memoize (fn [user-id challenge-id]
+             (binding [monger.core/*mongodb-database* (monger.core/get-db "score")]
+               (let [result (monger.collection/find-one (events-collection challenge-id) { :entity_id user-id :entity_type "user_id" } [:joined])
+                     joined-date (.get result "joined")]
+                 (java.util.Date. (.getYear joined-date) (.getMonth joined-date) (.getDate joined-date)))))))
+
+(defn score-cursor [challenge-id score-name]
   (binding [monger.core/*mongodb-database* (monger.core/get-db "score")]
     (monger.collection/find (score-collection challenge-id score-name))))
 
-(defn joined [user-id challenge-id]
-  (binding [monger.core/*mongodb-database* (monger.core/get-db "score")]
-    (let [result (monger.collection/find-one (events-collection challenge-id) { :entity_id user-id :entity_type "user_id" } [:joined])
-          joined-date (.get result "joined")]
-      (java.util.Date. (.getYear joined-date) (.getMonth joined-date) (.getDate joined-date)))))
+(defn lazy-score-seq [cursor]
+  (if (.hasNext cursor)
+    (cons (.next cursor) (lazy-seq (lazy-score-seq cursor)))
+    nil))
+
+(defn score-seq [challenge-id score-name]
+  (lazy-seq (lazy-score-seq (score-cursor challenge-id score-name))))
--- cohort/src/cohort/reports.clj
+++ cohort/src/cohort/reports.clj
@@ -1,29 +1,21 @@
 (ns cohort.reports
   (:use cohort.mongo))
 
-(defn add-daily-value [map day workouts joined]
-  (let [cohort-map (get map joined)]
-    (if (contains? cohort-map day)
-      (let [new-value (+ (get (get map joined) day) workouts)]
-        (assoc map joined (assoc cohort-map day new-value)))
-      (assoc map joined (assoc cohort-map day workouts)))))
+(defn update! [map score challenge-id score-key]
+  (let [workout-date (.get score "d")
+        user-id (.get score "i")
+        workouts (.get score score-key)
+        cohort-date (joined user-id challenge-id)]
+    (if (contains? map cohort-date)
+      (let [workout-map (get map cohort-date)]
+        (if (contains? workout-map workout-date)
+          (assoc! workout-map workout-date (+ workouts (get workout-map workout-date)))
+          (assoc! workout-map workout-date workouts)))
+      (assoc! map cohort-date (transient { workout-date workouts })))))
 
-(defn init-daily-value [map day workouts joined]
-  (assoc map joined (sorted-map day workouts)))
-
-(defn add-daily [map score challenge-id]
-  (let [user-id (.get score "i")
-        day (.get score "d")
-        workouts (.get score "s1")
-        user-joined (joined user-id challenge-id)]
-    (if (contains? map user-joined)
-      (add-daily-value map day workouts user-joined)
-      (init-daily-value map day workouts user-joined))))
-
-(defn daily-workout-real [challenge-id cursor accum]
-  (if (.hasNext cursor)
-    (recur challenge-id cursor (add-daily accum (.next cursor) challenge-id))
-    accum))
-
-(defn daily-workout-cohorts [challenge-id]
-  (daily-workout-real challenge-id (daily-score-cursor challenge-id "daily_cdphp") (sorted-map)))
+(defn workout-cohorts [challenge-id score-name score-key]
+  (loop [cursor (score-cursor challenge-id score-name)
+         result (transient (hash-map))]
+    (if (not (.hasNext cursor))
+      (persistent! result)
+      (recur cursor (update! result (.next cursor) challenge-id score-key)))))
