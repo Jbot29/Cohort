7f5c438acdd307efd39acf6aa675a8bcbea25e31
Carl Youngblood <carl.youngblood@ITs-MacBook-Pro.local>
2013-05-25 12:54:59 -0500

    another +1 to clojure skill

--- cohort/src/cohort/reports.clj
+++ cohort/src/cohort/reports.clj
@@ -2,59 +2,21 @@
   (:use cohort.mongo)
   (:require clojure.pprint))
 
-(defn scoreinit [^com.mongodb.DBObject score score-date score-keys]
-  (let [result (java.util.HashMap.)]
-    (.put result score-date (scoreget score score-keys))
-    result))
-
-(defn scoreget [^com.mongodb.DBObject score score-keys]
-  (for [key score-keys]
-    (.get score key)))
-
-(defn scoreadd [score-one score-two]
-  (for [[s-one s-two] (map vector score-one score-two)]
-    (+ s-one s-two)))
-
-(defn update [^java.util.HashMap map ^com.mongodb.DBObject score challenge-id score-keys]
-  (let [score-date (.get score "d")
-        user-id (.get score "i")
-        joined-date (joined user-id challenge-id)]
-    (if (.containsKey map joined-date)
-      (let [^java.util.HashMap joined-date-data (.get map joined-date)]
-        (if (.containsKey joined-date-data score-date)
-          (.put joined-date-data score-date (scoreadd (.get joined-date-data score-date) (scoreget score score-keys)))
-          (.put joined-date-data score-date (scoreget score score-keys))))
-      (.put map joined-date (scoreinit score score-date score-keys)))))
-
-(defn find-all-dates [^java.util.HashMap map]
-  (set
-    (for [[cohort cohort-data] map
-          [date scores] cohort-data]
-      date)))
-
-(defn listify [^java.util.HashMap map score-keys]
-  (let [all-dates (find-all-dates map)
-        zeroes (for [key score-keys] 0)]
-    (for [[cohort ^java.util.HashMap cohort-data] map
-          date all-dates]
-      (if (.containsKey cohort-data date)
-        (list cohort date (.get cohort-data date))
-        (list cohort date zeroes)))))
-
-(defn cmp [list-one list-two]
-  (let [cohort-one (first list-one)
-        cohort-two (first list-two)
-        cohort-compare (compare cohort-one cohort-two)]
-    (if (= cohort-compare 0)
-      (compare (second list-one) (second list-two))
-      cohort-compare)))
-
 (defn joined-cohorts [challenge-id score-name score-keys]
-  (let [result (java.util.HashMap.)
-        ^com.mongodb.DBCursor cursor (score-cursor challenge-id score-name)]
-    (while (.hasNext cursor)
-      (update result (.next cursor) challenge-id score-keys))
-    (sort cmp (listify result score-keys))))
+  (let [^com.mongodb.DBCursor scores (score-cursor challenge-id score-name)
+        score-fn (fn [^com.mongodb.DBObject score] (for [key score-keys] (.get score key)))
+        map-fn (fn [^com.mongodb.DBObject score] { (joined (.get score "i") challenge-id) { (.get score "d") (score-fn score) } })
+        cohort-map (apply merge-with (concat [(partial merge-with (partial map +))] (map map-fn scores)))
+        all-dates (set (mapcat keys (vals cohort-map)))
+        zeroes (for [key score-keys] 0)
+        sort-fn (fn [x y] (let [c (compare (first x) (first y))] (if (= c 0) (compare (second x) (second y)) c)))]
+    (sort sort-fn
+      (for [[cohort cohort-data] cohort-map
+            date all-dates]
+        (if (contains? cohort-data date)
+          (list cohort date (cohort-data date))
+          (list cohort date zeroes))))))
 
-(defn print-joined-cohorts [& args]
-  (clojure.pprint/pprint (apply joined-cohorts args)))
+(defn pprint-joined-cohorts [& args]
+  (clojure.pprint/pprint
+    (apply joined-cohorts args)))
