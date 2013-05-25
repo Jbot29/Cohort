3da94fb06f6fdadca87c29495e919924a77ebf56
Carl Youngblood <carl.youngblood@ITs-MacBook-Pro.local>
2013-05-24 20:03:19 -0500

    carl learns clojure +1

--- cohort/src/cohort/mongo.clj
+++ cohort/src/cohort/mongo.clj
@@ -9,21 +9,13 @@
 (defn score-collection [challenge-id score-name]
   (str "challenge_" challenge-id "_" score-name "_scores"))
 
-(defn joined [user-id challenge-id]
+(def joined
   (memoize (fn [user-id challenge-id]
              (binding [monger.core/*mongodb-database* (monger.core/get-db "score")]
                (let [result (monger.collection/find-one (events-collection challenge-id) { :entity_id user-id :entity_type "user_id" } [:joined])
-                     joined-date (.get result "joined")]
+                     ^java.util.Date joined-date (.get result "joined")]
                  (java.util.Date. (.getYear joined-date) (.getMonth joined-date) (.getDate joined-date)))))))
 
 (defn score-cursor [challenge-id score-name]
   (binding [monger.core/*mongodb-database* (monger.core/get-db "score")]
     (monger.collection/find (score-collection challenge-id score-name))))
-
-(defn lazy-score-seq [cursor]
-  (if (.hasNext cursor)
-    (cons (.next cursor) (lazy-seq (lazy-score-seq cursor)))
-    nil))
-
-(defn score-seq [challenge-id score-name]
-  (lazy-seq (lazy-score-seq (score-cursor challenge-id score-name))))
--- cohort/src/cohort/reports.clj
+++ cohort/src/cohort/reports.clj
@@ -1,21 +1,60 @@
 (ns cohort.reports
-  (:use cohort.mongo))
+  (:use cohort.mongo)
+  (:require clojure.pprint))
 
-(defn update! [map score challenge-id score-key]
-  (let [workout-date (.get score "d")
+(defn scoreinit [^com.mongodb.DBObject score score-date score-keys]
+  (let [result (java.util.HashMap.)]
+    (.put result score-date (scoreget score score-keys))
+    result))
+
+(defn scoreget [^com.mongodb.DBObject score score-keys]
+  (for [key score-keys]
+    (.get score key)))
+
+(defn scoreadd [score-one score-two]
+  (for [[s-one s-two] (map vector score-one score-two)]
+    (+ s-one s-two)))
+
+(defn update [^java.util.HashMap map ^com.mongodb.DBObject score challenge-id score-keys]
+  (let [score-date (.get score "d")
         user-id (.get score "i")
-        workouts (.get score score-key)
-        cohort-date (joined user-id challenge-id)]
-    (if (contains? map cohort-date)
-      (let [workout-map (get map cohort-date)]
-        (if (contains? workout-map workout-date)
-          (assoc! workout-map workout-date (+ workouts (get workout-map workout-date)))
-          (assoc! workout-map workout-date workouts)))
-      (assoc! map cohort-date (transient { workout-date workouts })))))
+        joined-date (joined user-id challenge-id)]
+    (if (.containsKey map joined-date)
+      (let [^java.util.HashMap joined-date-data (.get map joined-date)]
+        (if (.containsKey joined-date-data score-date)
+          (.put joined-date-data score-date (scoreadd (.get joined-date-data score-date) (scoreget score score-keys)))
+          (.put joined-date-data score-date (scoreget score score-keys))))
+      (.put map joined-date (scoreinit score score-date score-keys)))))
 
-(defn workout-cohorts [challenge-id score-name score-key]
-  (loop [cursor (score-cursor challenge-id score-name)
-         result (transient (hash-map))]
-    (if (not (.hasNext cursor))
-      (persistent! result)
-      (recur cursor (update! result (.next cursor) challenge-id score-key)))))
+(defn find-all-dates [^java.util.HashMap map]
+  (set
+    (for [[cohort cohort-data] map
+          [date scores] cohort-data]
+      date)))
+
+(defn listify [^java.util.HashMap map score-keys]
+  (let [all-dates (find-all-dates map)
+        zeroes (for [key score-keys] 0)]
+    (for [[cohort ^java.util.HashMap cohort-data] map
+          date all-dates]
+      (if (.containsKey cohort-data date)
+        (list cohort date (.get cohort-data date))
+        (list cohort date zeroes)))))
+
+(defn cmp [list-one list-two]
+  (let [cohort-one (first list-one)
+        cohort-two (first list-two)
+        cohort-compare (compare cohort-one cohort-two)]
+    (if (= cohort-compare 0)
+      (compare (second list-one) (second list-two))
+      cohort-compare)))
+
+(defn joined-cohorts [challenge-id score-name score-keys]
+  (let [result (java.util.HashMap.)
+        ^com.mongodb.DBCursor cursor (score-cursor challenge-id score-name)]
+    (while (.hasNext cursor)
+      (update result (.next cursor) challenge-id score-keys))
+    (sort cmp (listify result score-keys))))
+
+(defn print-joined-cohorts [& args]
+  (clojure.pprint/pprint (apply joined-cohorts args)))
